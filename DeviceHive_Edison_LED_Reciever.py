827c68ed87464da8f561fe9a677bda0e6e3152ad
PhilippFuraev <furaev.philipp@gmail.com>
2018-11-15 20:53:36 +0300

    version 0.13

--- DeviceHive_Edison_LED_Reciever.py
+++ DeviceHive_Edison_LED_Reciever.py
@@ -25,14 +25,14 @@ class ReceiverHandler(Handler):
         print('Notification "%s" received' % notification.notification)
         led = mraa.Gpio(2)
         led.dir(mraa.DIR_OUT)
-        if notification.notification[5:7] == 'on':
+        if notification.notification[4:5] == 'ON':
             self._device.data = {'LED': 'ON'}
             led.write(1)
             print("LED is ON!")
             self._device.save()
         else:
-            if (notification.notification[5:8] == 'off'):
-                self._device.data = {'TV': 'OFF'}
+            if (notification.notification[4:6] == 'OFF'):
+                self._device.data = {'LED': 'OFF'}
                 led.write(0)
                 print("LED is OFF")
                 self._device.save()
